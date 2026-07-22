50a30ee1652a9b9d18921631dbdf348700d26189
YakezDA <yakezdaughtry@gmail.com>
2026-07-21 20:25:08 -0600

    Add employee management functions and menu

--- app/main.py
+++ app/main.py
@@ -1,5 +1,68 @@
 from employee import Employee
 
-jake = Employee("Jake", "Doe", 25, 1234)
+employees = []
 
-print(jake.first_name)
+jake = Employee("Jake", "Doe", 25, 1234, "Manager")
+jill = Employee("Jill", "Doe", 25, 1235, "Assistant Manager")
+employees.append(jake)
+employees.append(jill)
+
+print("Employees:")
+
+for employee in employees:
+    print(employee.full_name())
+
+
+# search employee by id
+def search_employee_by_id(employee_id):
+    for employee in employees:
+        if employee.employee_id == employee_id:
+            return employee
+    return None
+
+
+# clock in employee
+def clock_in_employee():
+    employee_id = int(input("Enter the employee ID: "))
+    searched_employee = search_employee_by_id(employee_id)
+
+    if not searched_employee:
+        print("Employee not found.")
+        return
+
+    searched_employee.clock_in()
+
+
+# clock out employee
+def clock_out_employee():
+    employee_id = int(input("Enter the employee ID: "))
+    searched_employee = search_employee_by_id(employee_id)
+
+    if not searched_employee:
+        print("Employee not found.")
+        return
+
+    searched_employee.clock_out()
+
+
+def call_off_employee():
+    employee_id = int(input("Enter the employee ID: "))
+    searched_employee = search_employee_by_id(employee_id)
+
+    if not searched_employee:
+        print("Employee not found.")
+        return
+
+    searched_employee.call_off()
+
+
+def display_menu():
+    print("==== Zoo Ops Menu ====")
+    print("1. Clock In Employee")
+    print("2. Clock Out Employee")
+    print("3. Call Off Employee")
+    print("4. Employee List")
+    print("5. Exit")
+
+
+display_menu()
